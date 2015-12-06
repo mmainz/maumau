16d7e57a9a971ba0ac53e1972683e81f03cdcbdd
Mario Mainz <mario.mainz@invision.de>
2015-12-07 00:30:58 +0100

    Add primitive debugging UI

--- src/maumau/core.cljs
+++ src/maumau/core.cljs
@@ -1,6 +1,7 @@
 (ns maumau.core
-  (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]
+  (:require [goog.dom :as gdom]
+            [om.next :as om :refer-macros [defui]]
+            [om.dom :as dom]
             [maumau.cards :as cards]
             [maumau.game :as game]))
 
@@ -9,10 +10,24 @@
 (defonce app-state
   (atom {:game-state (game/init-game-state 2)}))
 
-(om/root
- (fn [data owner]
-   (reify om/IRender
-     (render [_]
-       (dom/h1 nil (:text data)))))
- app-state
- {:target (. js/document (getElementById "app"))})
+(defui App
+  Object
+  (render [this]
+          (let [game-state (:game-state (om/props this))
+                {:keys [drawing-pile playing-pile players]} game-state]
+            (dom/div nil
+                     (dom/div nil (str "Drawing pile cards: "
+                                       (count drawing-pile)))
+                     (dom/div nil (str "Played cards: "
+                                       (count playing-pile)))
+                     (dom/div nil (str "Players: "
+                                       (count players)))))))
+
+(def app (om/factory App))
+
+(def reconciler
+  (om/reconciler {:state app-state}))
+
+(om/add-root! reconciler
+              App
+              (gdom/getElement "app"))
